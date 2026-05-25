7d1620eaccdec0e4f507654b901cec225c60ae9e
JhonatanGAlves <jhonatangalves96@gmail.com>
2026-05-24 22:33:53 -0300

    🔔 feat(frontend): setup react-hot-toast for global feedback notifications
    - Install react-hot-toast
    - Mount Toaster at top-right in main.tsx

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { ApolloProvider } from '@apollo/client/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Toaster } from 'react-hot-toast'
 import { BrowserRouter } from 'react-router-dom'
 
 import { App } from '@/App'
@@ -19,6 +20,7 @@ createRoot(document.getElementById('root')!).render(
         <BrowserRouter>
           <AuthProvider>
             <App />
+            <Toaster position="bottom-right" />
           </AuthProvider>
         </BrowserRouter>
       </QueryClientProvider>
